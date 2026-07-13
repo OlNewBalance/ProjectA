ac8b55921c96757e7003b2d6becb875765c353c5
Matvey Volkov <mavolkov@uma.tech>
2026-07-13 09:49:23 +0400

    PRA-6: coins

---
+++ Assets/Source/Pickup/LevelCoin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Source.Pickup
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class LevelCoin: MonoBehaviour, IPickup
+    {
+        private int _expValue;
+
+        private void Awake()
+        {
+            _expValue = G.CurrentCoinExpValue;
+        }
+
+        public void OnPickup()
+        {
+            G.CurrentExp += _expValue;
+        }
+    }
+}
