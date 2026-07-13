d9bc7b566325fed76a99af34c0d6a9cc72ea5cc5
Matvey Volkov <mavolkov@uma.tech>
2026-07-13 09:04:50 +0400

    Inint struct and actions for G

--- Assets/Source/G.cs
+++ Assets/Source/G.cs
@@ -1,13 +1,54 @@
-﻿namespace Source
+﻿using System;
+using Unity.VisualScripting;
+
+namespace Source
 {
     public static class G
     {
-        public static void InitG()
+        public static GInit DefaultInit =  new GInit(
+            level: 1, 
+            exp: 0);
+        
+        public static void InitG(GInit gInit)
         {
-            
+            CurrentLevel = gInit.CurrentLevel;
+            CurrentExp = gInit.CurrentExp;
+        }
+
+        private static int _currentLevel;
+        public static Action<int> OnLevelChanged;
+
+        public static int CurrentLevel
+        {
+            get => _currentLevel;
+            set { 
+                _currentLevel = value; 
+                OnLevelChanged?.Invoke(_currentLevel); 
+            }
+        }
+        private static int _currentExp;
+
+        public static Action<int> OnExpChanged;
+
+        public static int CurrentExp
+        {
+            get => _currentExp;
+            set {
+                _currentExp = value;
+                OnExpChanged?.Invoke(_currentExp);
+            }
+        }
+    }
+
+    public struct GInit
+    {
+        public  GInit(int level, int exp)
+        {
+            CurrentLevel = level;
+            CurrentExp = exp;
         }
         
-        public static int CurrentLevel = 1;
-        public static float CurrentExp;
+        public int CurrentLevel;
+        public int CurrentExp;
     }
 }
---
+++ Assets/Source/Pickup/IPickup.cs
@@ -0,0 +1,8 @@
+﻿namespace Source.Pickup
+{
+    // Only for players pickups
+    public interface IPickup
+    {
+        void OnPickup();
+    }
+}
